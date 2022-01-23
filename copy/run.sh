5a04d3f60192cd28c19a22972c0e0ba30bebd054
GG <guillaume.guarino@insa-strasbourg.fr>
2022-01-23 09:24:49 +0100

    modify dockerfile

--- copy/run.sh
+++ copy/run.sh
@@ -1,3 +1,3 @@
-echo "Allow 10 sec for JupyterLab to start @ http://192.168.55.1:8888 (password nvidia) or @ http://jetson_ip:8888 if wifi connection was set up."
+echo "Allow 10 sec for JupyterLab to start @ http://192.168.55.1:$1 (password nvidia) or @ http://jetson_ip:$1 if wifi connection was set up."
 nohup jupyter lab --ip 0.0.0.0 --allow-root --no-browser > /var/log/jupyter.log 2>&1 &
 /bin/bash
--- drun.sh
+++ drun.sh
@@ -1,6 +1,19 @@
 # Prepare target env
-CONTAINER_DISPLAY="0"
+CONTAINER_DISPLAY=$(ps aux | grep -c docker-proxy)
 CONTAINER_HOSTNAME="root"
+
+# Find open port for jupyterlab
+port=8888
+quit=0
+
+while [ "$quit" -ne 1 ]; do
+  netstat -a | grep $port >> /dev/null
+  if [ $? -gt 0 ]; then
+    quit=1
+  else
+    port=`expr $port + 1`
+  fi
+done
 
 # Create a directory for the socket
 mkdir -p display/socket
@@ -11,14 +24,12 @@ DISPLAY_NUMBER=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
 
 # Extract current authentication cookie
 AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUMBER} " | awk '{print $3}')
-echo $AUTH_COOKIE
 
 # Create the new X Authority file
 xauth -f display/Xauthority add ${CONTAINER_HOSTNAME}/unix:${CONTAINER_DISPLAY} MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
 
 # Proxy with the :0 DISPLAY
-sudo rm -rf display/socket
-mkdir -p display/socket
+sudo rm -rf display/socket/X${CONTAINER_DISPLAY} || true
 socat UNIX-LISTEN:display/socket/X${CONTAINER_DISPLAY},fork TCP4:localhost:60${DISPLAY_NUMBER} &
 
 # Launch the container
@@ -27,6 +38,7 @@ docker run -it --rm \
   --gpus all \
   -e DISPLAY=:${CONTAINER_DISPLAY} \
   -e XAUTHORITY=/tmp/.Xauthority \
+  -e JPORT=${port} \
   --device /dev/snd \
   --device /dev/bus/usb \
   --cap-add SYS_PTRACE \
@@ -35,7 +47,7 @@ docker run -it --rm \
   -v ${PWD}/display/Xauthority:/tmp/.Xauthority \
   -v /tmp/argus_socket:/tmp/argus_socket \
   -v ${PWD}:/app \
-  -p 8888:8888 \
+  -p $port:8888 \
   --privileged \
   --hostname ${CONTAINER_HOSTNAME} \
   jetson_c
