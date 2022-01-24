a2c654b4e9e391e0303d5a049e78df0ef12e9221
Your Name <you@example.com>
2022-01-24 23:39:43 +0100

    tensorrt

--- src/tensorrt/sound_processing.py
+++ src/tensorrt/sound_processing.py
@@ -9,7 +9,7 @@ from torch2trt import TRTModule
 
 from tensorrt_ import build_engine, load_engine, infer_with_trt, init_trt_buffers
 
-task='no TensorRT'
+task='TensorRT' # run first with no TensoRT to export model
 DYNAMIC_SIZES = False
 ENGINE_FILE_PATH = '../../models/wav2vec.trt'
 ONNX_FILE_PATH = '../../models/wav2vec.onnx'
@@ -24,7 +24,7 @@ if task=='no TensorRT':
     
     input_audio, _ = librosa.load(FILENAME, sr=16000)
     # boucle pour tous les traitements
-    for _ in range(100):
+    for _ in range(1):
         input_ = tokenizer(input_audio, sampling_rate=16000, return_tensors="pt").input_values[... ,:MAX_INPUT_SIZE]
         start = time.time()
         logits = model(input_.cuda()).logits.cpu() #on envoit les données sur gpu puis on transfère les résultats au cpu
@@ -33,28 +33,30 @@ if task=='no TensorRT':
         predicted_ids = torch.argmax(logits, dim=-1)
         transcription = tokenizer.batch_decode(predicted_ids)[0]
         print(transcription)
-
+    
+    ## This is not supposed to be done on a Jetson
     # Saving model
-    # indicates that the dimension 1 of input can vary
-    if DYNAMIC_SIZES:
-        dynamic_axes = {'input': {1: 'len_sound'}}
-    else:
-        dynamic_axes = None
     print("Exporting model...")
-    torch.onnx.export(model, input_.cuda()[..., 5000] , ONNX_FILE_PATH, input_names=['input'],
-                      output_names=['output'], dynamic_axes=dynamic_axes, export_params=True)
-
+    dummy_input = input_
+    if DYNAMIC_SIZES:
+        # indicates that the dimension 1 of input can vary
+        dynamic_axes = {'input': {1: 'len_sound'}}
+        torch.onnx.export(model, dummy_input[... ,:1000].cuda() , ONNX_FILE_PATH, input_names=['input'],
+                          output_names=['output'], dynamic_axes=dynamic_axes, export_params=True)
+    else:
+        torch.onnx.export(model, dummy_input.cuda(), ONNX_FILE_PATH, input_names=['input'],
+                          output_names=['output'], export_params=True)
     onnx_model = onnx.load(ONNX_FILE_PATH)
     #onnx.checker.check_model(onnx_model)
 
     # Serializing model   
     print("Serializing model...")
-    build_engine(ONNX_FILE_PATH, ENGINE_FILE_PATH, MAX_INPUT_SIZE)
+    build_engine(ONNX_FILE_PATH, ENGINE_FILE_PATH, MAX_INPUT_SIZE, DYNAMIC_SIZES)
     print("Done.")
 
 elif task=='TensorRT': 
     # initialize TensorRT engine
-    engine = load_engine(ENGINE_FILE_PATH, ONNX_FILE_PATH, dynamic)
+    engine = load_engine(ENGINE_FILE_PATH, ONNX_FILE_PATH)
 
     input_audio, _ = librosa.load(FILENAME, sr=16000)
     tokenizer = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
