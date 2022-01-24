32772884027f107146f82b4b9353c608ca49a384
Your Name <you@example.com>
2022-01-25 00:33:11 +0100

    tensorrt

--- src/tensorrt/sound_processing.py
+++ src/tensorrt/sound_processing.py
@@ -14,7 +14,7 @@ DYNAMIC_SIZES = False
 ENGINE_FILE_PATH = '../../models/wav2vec.trt'
 ONNX_FILE_PATH = '../../models/wav2vec.onnx'
 FILENAME = '../../data/Open_test.wav'
-MAX_INPUT_SIZE = 75000
+MAX_INPUT_SIZE = 80000
 
 if task == 'no TensorRT':
     tokenizer = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
