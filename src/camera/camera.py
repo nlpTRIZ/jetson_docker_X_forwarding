2e613b32754bb423d92637b6ece8f18153b5f875
Your Name <you@example.com>
2022-01-30 10:35:14 +0100

    add humanpose

--- src/camera/camera.py
+++ src/camera/camera.py
@@ -1,98 +1,36 @@
-# MIT License
-# Copyright (c) 2019 JetsonHacks
-# See license
-# Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
-# NVIDIA Jetson Nano Developer Kit using OpenCV
-# Drivers for the camera and OpenCV are included in the base image
-
-import cv2
-
-# gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
-# Defaults to 1280x720 @ 60fps
-# Flip the image by setting the flip_method (most common values: 0 and 2)
-# display_width and display_height determine the size of the window on the screen
-
-
-def gstreamer_pipeline(
-    capture_width=1280,
-    capture_height=720,
-    display_width=1280,
-    display_height=720,
-    framerate=60,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
-
-def show_camera():
-    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline(flip_method=0))
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-    if cap.isOpened():
-        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
-        # Window
-        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
-            ret_val, img = cap.read()
-            cv2.imshow("CSI Camera", img)
-            # This also acts as
-            keyCode = cv2.waitKey(30) & 0xFF
-            # Stop the program on the ESC key
-            if keyCode == 27:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
-    else:
-        print("Unable to open camera")
-
-
-#if __name__ == "__main__":
-#    show_camera()
-
-import jetcam
-
-# from jetcam.usb_camera import USBCamera
 from jetcam.csi_camera import CSICamera
 from jetcam.utils import bgr8_to_jpeg
 
-# camera = USBCamera(width=WIDTH, height=HEIGHT, capture_fps=30)
 WIDTH=1080
 HEIGHT=720
-camera = CSICamera(width=WIDTH, height=HEIGHT, capture_fps=30)
+camera = CSICamera(width=WIDTH, height=HEIGHT, capture_fps=12)
+camera.running=True
 
-camera.running = True
+# Create window
+import cv2
+cv2.namedWindow('TEST', cv2.WINDOW_AUTOSIZE)
 
-window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+#Mouse callback function to be executed when a mouse event take place (double click)
+def mouse_click (event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        camera.unobserve_all()
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
+        print ("Closing window.")
+        #Close camera
+        camera.running = False
+        camera.cap.release()
 
-global image
+cv2.setMouseCallback('TEST', mouse_click)
+
+#Main function
 def execute(change):
     image = change['new']
-
-execute({'new': camera.value})
-
+    ###
+    # process
+    ###
+    cv2.imshow('TEST', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        pass
+    
 camera.observe(execute, names='value')
-
-while True:
-    cv2.imshow("CSI Camera", image)
-    kk = cv2.waitKey(1)
-    if kk == ord('q'):  # exit with key q
-        break
-
-camera.unobserve_all()
-cv2.destroyAllWindows()
