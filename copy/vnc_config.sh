cd960bdbee4081b1f64fc6fbec805659e9340976
nlpTRIZ <67756737+nlpTRIZ@users.noreply.github.com>
2022-11-17 12:27:50 +0100

    Update vnc_config.sh

--- copy/vnc_config.sh
+++ copy/vnc_config.sh
@@ -1,1 +1,6 @@
-
+mkdir -p ~/.config/autostart
+cp /usr/share/applications/vino-server.desktop ~/.config/autostart
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password $(echo -n 'jetson0insa'|base64)
