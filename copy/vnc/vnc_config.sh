2855de4ee70d201e8a47b54948b8cf0f10842dce
Gigi <iamaman>
2022-11-17 12:58:26 +0100

    add vnc support

--- copy/vnc/vnc_config.sh
+++ copy/vnc/vnc_config.sh
@@ -5,5 +5,5 @@ gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
 gsettings set org.gnome.Vino vnc-password $(echo -n '$(hostname)insa'|base64)
 sudo cp copy/vnc/xorg.conf /etc/X11/xorg.conf
-sudo cp copy/vnc/custom.conf /etc/gdm3/custom.conf
+sudo echo "$(copy/vnc/custom.conf)" > /etc/gdm3/custom.conf
 sudo reboot
