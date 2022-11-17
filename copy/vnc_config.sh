5233d42ef2d5712eb748ae6ce2125edd3d35e0c1
nlpTRIZ <67756737+nlpTRIZ@users.noreply.github.com>
2022-11-17 12:34:10 +0100

    Update vnc_config.sh

--- copy/vnc_config.sh
+++ copy/vnc_config.sh
@@ -6,3 +6,4 @@ gsettings set org.gnome.Vino authentication-methods "['vnc']"
 gsettings set org.gnome.Vino vnc-password $(echo -n 'jetson0insa'|base64)
 sudo cp copy/vnc/xorg.conf /etc/X11/xorg.conf
 sudo cp copy/vnc/custom.conf /etc/gdm3/custom.conf
+sudo reboot
